241081ffe79b2dd2b3a89f0344f5b50de791db5e
Fadlin Afina <fadlinafinab@gmail.com>
2025-12-12 15:50:39 +0800

    Add files via upload

---
+++ database/migrations/2025_11_10_195421_create_forum_posts_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('forum_posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->longText('content');
+
+            $table->unsignedBigInteger('author_id')->nullable();
+            $table->string('author_name')->default('Demo User');
+            $table->string('author_avatar')->default('/images/default-user.png');
+
+            $table->string('attachment')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('forum_posts');
+    }
+};
